36586476d714f50ff9846c58a46a6bbceda274a8
Meisam Farsi <3268953+brra@users.noreply.github.com>
2021-08-13 02:39:41 +0200

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+cp .vimrc /root/.vimrc
+cp .bash_aliases /root/.bash_aliases
+cp .bashrc-root /root/.bashrc 
+cp .tmux.conf /root/.tmux.conf 
+cp .vimrc /root/.vimrc 
+cp nanorc /etc/nanorc
+cp .bashrc /home/brra/.bashrc 
+cp .vimrc /home/brra/.vimrc 
+cp .bash_aliases /home/brra/.bash_aliases
+cp .tmux.conf /home/brra/.tmux.conf 
+cp .vimrc /home/brra/.vimrc 
+chown brra:brra -R /home/brra/
+
+sh system_settings.se 
+sh Install_apps.sh
+sh docker_manual.sh
+sh get_tailscale.sh
