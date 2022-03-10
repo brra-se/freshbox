912e2365dd2899adf201ec70120f7b12f98c3849
Meisam Farsi <3268953+brra@users.noreply.github.com>
2022-03-10 18:00:02 +0100

    Update system_settings.sh
    remooved sudo

--- system_settings.sh
+++ system_settings.sh
@@ -19,65 +19,14 @@ echo "## Use with care.
 ## https://www.whonix.org/wiki/Dev/Permissions
 %sudo   ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/user-passwordless
 
-sudo bash -c "cat >> /etc/sysctl.conf" << EOF
-net.ipv4.tcp_slow_start_after_idle=0
-net.core.rmem_max=16777216
-fs.inotify.max_user_watches=1048576
-kernel.softlockup_all_cpu_backtrace=1
-kernel.softlockup_panic=1
-fs.file-max=2097152 
-fs.nr_open=2097152
-fs.may_detach_mounts=1
-fs.inotify.max_user_instances=8192
-fs.inotify.max_queued_events=16384
-vm.max_map_count=262144
-net.core.netdev_max_backlog=16384
-net.ipv4.tcp_wmem=4096 12582912 16777216
-net.core.wmem_max=16777216
-net.core.somaxconn=32768
-net.ipv4.ip_forward=1
-net.ipv4.tcp_timestamps=0
-net.ipv4.tcp_max_syn_backlog=8096
-net.bridge.bridge-nf-call-iptables=1
-net.bridge.bridge-nf-call-ip6tables=1
-net.bridge.bridge-nf-call-arptables=1
-net.ipv4.tcp_rmem=4096 12582912 16777216
-vm.swappiness=0
-kernel.sysrq=1
-net.ipv4.neigh.default.gc_stale_time=120
-net.ipv4.conf.all.rp_filter=0
-net.ipv4.conf.default.rp_filter=0
-net.ipv4.conf.default.arp_announce=2
-net.ipv4.conf.lo.arp_announce=2
-net.ipv4.conf.all.arp_announce=2
-net.ipv4.tcp_max_tw_buckets=5000
-net.ipv4.tcp_syncookies=1
-net.ipv4.tcp_synack_retries=2
-net.ipv6.conf.lo.disable_ipv6=1
-net.ipv6.conf.all.disable_ipv6=1
-net.ipv6.conf.default.disable_ipv6=1
-net.ipv4.ip_local_port_range=1024 65535
-net.ipv4.tcp_keepalive_time=600
-net.ipv4.tcp_keepalive_probes=10
-net.ipv4.tcp_keepalive_intvl=30
-net.ipv4.tcp_orphan_retries=3
-net.nf_conntrack_max=25000000
-net.netfilter.nf_conntrack_max=25000000
-net.netfilter.nf_conntrack_tcp_timeout_established=180
-net.netfilter.nf_conntrack_tcp_timeout_time_wait=120
-net.netfilter.nf_conntrack_tcp_timeout_close_wait=60
-net.netfilter.nf_conntrack_tcp_timeout_fin_wait=12
-EOF
 
-sudo sysctl -p
-
-sudo bash -c "cat >> /etc/default/grub" << EOF
+bash -c "cat >> /etc/default/grub" << EOF
 # disable ipv6
 GRUB_CMDLINE_LINUX_DEFAULT="ipv6.disable=1"
 GRUB_CMDLINE_LINUX="ipv6.disable=1"
 EOF
 
-sudo update-grub
+update-grub
 
 timedatectl set-timezone Europe/Stockholm
 echo 'Europe/Stockholm' /etc/timezone
