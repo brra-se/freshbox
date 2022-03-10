f3339e7bb7e8613ddc81971f82350f1716e7b1ae
Meisam Farsi <3268953+brra@users.noreply.github.com>
2022-03-10 18:02:45 +0100

    Create Install_apps-prox.sh

---
+++ Install_apps-prox.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+apt update && apt full-upgrade
+
+apt install -y  --no-install-recommends \
+apt-transport-https \
+ca-certificates \
+curl \
+dirmngr \
+gnupg-agent \
+software-properties-common \
+bash-completion \
+command-not-found \
+dialog \
+net-tools \
+git \
+gnupg2 \
+locales \
+autofs \
+lsb-release  \
+adduser \
+tmux \
+tmux-plugin-manager \
+gnupg \
+hostname  \
+python3  \
+python3-dev  \
+python3-pip
+
+apt -y install python python3-virtualenv
+pip install pip-upgrader
+pip install --upgrade pip
+
