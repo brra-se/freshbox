423387ab66820d060886ec56196851de0fed3c56
Meisam Farsi <3268953+brra@users.noreply.github.com>
2022-03-14 21:31:32 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,15 +6,15 @@ cp .bashrc-root /root/.bashrc
 cp .tmux.conf /root/.tmux.conf 
 cp .vimrc /root/.vimrc 
 cp nanorc /etc/nanorc
-cp .bashrc /home/brra/.bashrc 
-cp .vimrc /home/brra/.vimrc 
-cp .bash_aliases /home/brra/.bash_aliases
-cp .tmux.conf /home/brra/.tmux.conf 
-cp .vimrc /home/brra/.vimrc 
-chown brra:brra -R /home/brra/
+cp .bashrc ~/.bashrc 
+cp .vimrc ~/.vimrc 
+cp .bash_aliases ~/.bash_aliases
+cp .tmux.conf ~/.tmux.conf 
+cp .vimrc ~/.vimrc 
+#chown brra:brra -R /ubuntu/brra/
 
 sh system_settings.se 
 #sh Install_apps.sh
-sh Install_apps_prox.sh
-sh docker_manual.sh
+#sh Install_apps_prox.sh
+#sh docker_manual.sh
 #sh get_tailscale.sh
